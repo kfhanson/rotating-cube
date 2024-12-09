5545c8b238758b59740eb41b676007f4dc8a44f5
kfhanson <kfhanson.2508@gmail.com>
2024-12-10 01:23:00 +0700

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,13 @@
+#include <math.h>
+
+float A, B, C;
+
+float calX(int i, int j, int k){
+	return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) + j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+}
+
+float 
+
+int main(){
+	return 0;
+}
