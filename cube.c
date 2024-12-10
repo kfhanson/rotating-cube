75cb586e18d5db0d8dd4e34e8df107cdc92c5c2c
kfhanson <kfhanson.2508@gmail.com>
2024-12-10 14:29:45 +0700

    Continued calX(), calY(), calZ().

--- cube.c
+++ cube.c
@@ -1,13 +1,32 @@
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
-float A, B, C;
+float x, y, z;
+float cube = 10;
+int width = 160;
+int height = 44;
+float zBuffer[160*44];
+char buffer[160*44];
+int background = ' ';
 
 float calX(int i, int j, int k){
-	return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) + j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+	return j*sin(x)*sin(y)*cos(z)-k*cos(x)*sin(y)*cos(z)+k*sin(x)*sin(z)+j*cos(x)*sin(z)+i*cos(y)*cos(z);
 }
 
-float 
+float calY(int i, int j, int k){
+	return k*sin(x)*cos(z)+j*cos(x)*cos(z)-j*sin(x)*sin(y)*sin(z)+k*cos(x)*sin(y)*sin(z)-i*cos(y)*sin(z);
+}
+
+float calZ(int i, int j, int k){
+	return k*cos(x)*cos(y)-j*sin(x)*cos(y)+i*sin(y);
+}
 
 int main(){
+	printf("\x1b[2J");
+	while(1){
+		memset(buffer, background, width*height);
+		memset(zBuffer, 0, width * height * 4);
+	}
 	return 0;
 }
